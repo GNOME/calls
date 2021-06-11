9f9ea16509b8b0cdf85b38828472d6fee6c7df7a
Evangelos Ribeiro Tzaras <evangelos.tzaras@puri.sm>
2021-06-11 10:08:54 +0200

    dir-locals: Treat G_* macros as including a semicolon
    Otherwise without appending a semicolon explicitly to the macros, emacs
    will insist on indentation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
-((nil . ((indent-tabs-mode . nil))))
+((nil . ((indent-tabs-mode . nil)))
+ ;; thanks to Mohammed Sadiq, see https://source.puri.sm/Librem5/calls/-/merge_requests/332#note_159469
+ (c-mode . ((c-macro-names-with-semicolon
+             . ("G_BEGIN_DECLS" "G_END_DECLS" "G_DECLARE_FINAL_TYPE" "G_DEFINE_QUARK"
+                "G_DECLARE_DERIVABLE_TYPE" "G_DECLARE_INTERFACE" "G_DEFINE_TYPE"
+                "G_DEFINE_TYPE_WITH_PRIVATE" "G_DEFINE_ABSTRACT_TYPE"
+                "G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE")))))
