160d3e42a8f4983d0a2a9aee3160a7630a6280bf
Evangelos Ribeiro Tzaras <evangelos.tzaras@puri.sm>
2021-06-18 10:55:47 +0200

    ringer: Follow coding style for G_LOG_DOMAIN
    Closes #294

--- src/calls-ringer.c
+++ src/calls-ringer.c
@@ -21,7 +21,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
  */
-#define G_LOG_DOMAIN "calls-ringer"
+#define G_LOG_DOMAIN "CallsRinger"
 
 #include "calls-ringer.h"
 #include "calls-manager.h"
