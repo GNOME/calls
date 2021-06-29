49c347ca316a6af5c8e042d91421d2f2b6341951
Evangelos Ribeiro Tzaras <evangelos.tzaras@puri.sm>
2021-06-30 00:39:07 +0200

    meson: Add postinstall script to compile schemas etc
    Closes #298

---
+++ build-aux/meson/postinstall.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from os import environ, path
+from subprocess import call
+
+# Package managers set this, so we don't need to run
+if not environ.get('DESTDIR', ''):
+    prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+    datadir = path.join(prefix, 'share')
+
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+
+    print('Updating desktop database...')
+    call(['update-desktop-database', path.join(datadir, 'applications')])
+
+    print('Compiling schemas...')
+    call(['glib-compile-schemas', path.join(datadir, 'glib-2.0', 'schemas')])
