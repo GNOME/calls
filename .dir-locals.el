10a204654916d1dae49042f91ef695c68c3318ea
Evangelos Ribeiro Tzaras <evangelos.tzaras@puri.sm>
2021-07-09 17:47:39 +0200

    dir-locals: Treat more G_* macros as including a semicolon
    This makes emacs not insist on indendation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,13 @@
  ;; thanks to Mohammed Sadiq, see https://source.puri.sm/Librem5/calls/-/merge_requests/332#note_159469
  (c-mode . ((c-macro-names-with-semicolon
              . ("G_BEGIN_DECLS" "G_END_DECLS" "G_DECLARE_FINAL_TYPE" "G_DEFINE_QUARK"
-                "G_DECLARE_DERIVABLE_TYPE" "G_DECLARE_INTERFACE" "G_DEFINE_TYPE"
-                "G_DEFINE_TYPE_WITH_PRIVATE" "G_DEFINE_ABSTRACT_TYPE"
-                "G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE")))))
+                "G_DECLARE_DERIVABLE_TYPE" "G_DECLARE_INTERFACE"
+                "G_DEFINE_ABSTRACT_TYPE" "G_DEFINE_ABSTRACT_TYPE_WITH_CODE"
+                "G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE"
+                "G_DEFINE_AUTO_CLEANUP_CLEAR_FUNC" "G_DEFINE_AUTO_CLEANUP_FREE_FUNC"
+                "G_DEFINE_AUTOPTR_CLEANUP_FUNC"
+                "G_DEFINE_DYNAMIC_TYPE" "G_DEFINE_DYNAMIC_TYPE_EXTENDED"
+                "G_DEFINE_INTERFACE" "G_DEFINE_INTERFACE_WITH_CODE"
+                "G_DEFINE_TYPE" "G_DEFINE_TYPE_EXTENDED"
+                "G_DEFINE_TYPE_WITH_CODE" "G_DEFINE_TYPE_WITH_PRIVATE"
+                 )))))
