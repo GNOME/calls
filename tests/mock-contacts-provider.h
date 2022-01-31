72adee492293be2aa47f8727f9a96f80ae24ca4b
Evangelos Ribeiro Tzaras <devrtz@fortysixandtwo.eu>
2022-01-31 11:05:23 +0100

    tests: mock-contacts-provider: Add license header

--- tests/mock-contacts-provider.h
+++ tests/mock-contacts-provider.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Evangelos Ribeiro Tzaras <devrtz@fortysixandtwo.eu>
+ *
+ */
+
 #pragma once
 
 #include "calls-contacts-provider.h"
