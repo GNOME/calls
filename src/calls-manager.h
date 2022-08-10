21ae0a67c4c6e52dc6a4c182abb0fde6e7e03caf
Evangelos Ribeiro Tzaras <devrtz@fortysixandtwo.eu>
2022-08-10 15:07:02 +0200

    manager: Drop unused get_contact_name() declaration
    The function has been removed since quite some time,
    but the declaration did not get removed.
    
    Fixes 889d5a09817eb6f37a429edd25bffa1456d8e5ec

--- src/calls-manager.h
+++ src/calls-manager.h
@@ -66,7 +66,6 @@ GListModel            *calls_manager_get_suitable_origins     (CallsManager *sel
                                                                const char   *target);
 CallsOrigin           *calls_manager_get_origin_by_id         (CallsManager *self,
                                                                const char   *origin_id);
-const gchar           *calls_manager_get_contact_name         (CallsCall *call);
 gboolean               calls_manager_has_active_call          (CallsManager *self);
 void                   calls_manager_hang_up_all_calls        (CallsManager *self);
 gboolean               calls_manager_has_any_provider         (CallsManager *self);
