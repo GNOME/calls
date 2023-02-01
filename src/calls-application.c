GNOME/calls

commit e878fabb27a924aec1bbff0522dbf52a17158155

application: Remove and destroy application windows on shutdown
The windows need to be removed from the GtkApplication because they are
holding references to the application which prevents proper cleanup.

Fixes: #508

src/calls-application.c
@@ -79,14 +79,24 @@ G_DEFINE_TYPE (CallsApplication, calls_application, GTK_TYPE_APPLICATION);
 
 static void start_proper (CallsApplication *self);
 
+
+static void
+quit_calls (CallsApplication *self)
+{
+  gtk_application_remove_window (GTK_APPLICATION (self), GTK_WINDOW (self->main_window));
+  gtk_application_remove_window (GTK_APPLICATION (self), GTK_WINDOW (self->call_window));
+}
+
+
 static gboolean
 on_int_or_term_signal (CallsApplication *self)
 {
   g_debug ("Received SIGTERM/SIGINT, shutting down gracefully");
 
-  g_application_quit (G_APPLICATION (self));
   self->id_sigint = 0;
   self->id_sigterm = 0;
+
+  quit_calls (self);
 
   return G_SOURCE_REMOVE;
 }
@@ -676,8 +686,9 @@ finalize (GObject *object)
   g_clear_handle_id (&self->id_sigterm, g_source_remove);
   g_clear_handle_id (&self->id_sigint, g_source_remove);
 
-  g_clear_object (&self->call_window);
-  g_clear_object (&self->main_window);
+  gtk_widget_destroy (GTK_WIDGET (self->main_window));
+  gtk_widget_destroy (GTK_WIDGET (self->call_window));
+
   g_clear_object (&self->record_store);
   g_clear_object (&self->ringer);
   g_clear_object (&self->notifier);

commit 0c004b520406c9ac61c15d1f5033060b2f60b748

application: Only destroy previously created widgets
If this is not the primary instance self->main_window and
self->call_window will not have been created in the first place.

Fixes: e878fabb27a924aec1bbff0522dbf52a17158155

src/calls-application.c
@@ -686,8 +686,10 @@ finalize (GObject *object)
   g_clear_handle_id (&self->id_sigterm, g_source_remove);
   g_clear_handle_id (&self->id_sigint, g_source_remove);
 
-  gtk_widget_destroy (GTK_WIDGET (self->main_window));
-  gtk_widget_destroy (GTK_WIDGET (self->call_window));
+  if (self->main_window)
+    gtk_widget_destroy (GTK_WIDGET (self->main_window));
+  if (self->call_window)
+    gtk_widget_destroy (GTK_WIDGET (self->call_window));
 
   g_clear_object (&self->record_store);
   g_clear_object (&self->ringer);