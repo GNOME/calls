ca059116b991244e6bc1b9cb8965fdfde68239ce
Anton Lazarev <antonok35@gmail.com>
2023-12-13 17:12:38 -0800

    calls-application: Stop using GdkScreen
    https://docs.gtk.org/gtk4/migrating-3to4.html#adapt-to-gtkwidgets-size-allocation-changes
    
    Part-of: <https://gitlab.gnome.org/GNOME/calls/-/merge_requests/714>

--- src/calls-application.c
+++ src/calls-application.c
@@ -510,9 +510,9 @@ startup (GApplication *application)
 
   provider = gtk_css_provider_new ();
   gtk_css_provider_load_from_resource (provider, "/org/gnome/Calls/style.css");
-  gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
-                                             GTK_STYLE_PROVIDER (provider),
-                                             GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+  gtk_style_context_add_provider_for_display (gdk_display_get_default (),
+                                              GTK_STYLE_PROVIDER (provider),
+                                              GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
 
